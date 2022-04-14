729299b34cb229f402b3cefadb9faba5a84edf20
Alphardovo <98483817+Alphardovo@users.noreply.github.com>
2022-04-14 04:06:18 +0000

    lab6

--- answers.md
+++ answers.md
@@ -1,20 +1,36 @@
 # CMPS 2200 Reciation 6
 ## Answers
 
-**Name:**_________________________
+**Name:**__Maria Chen / Haochen Chen_______________________
 
 
 Place all written answers from `recitation-06.md` here for easier grading.
 
 
 
+- **1a.**
 
-
+For an array that has already been sorted, if you choose to use quick sort and use the first element as the reference, because the reference selected each time is the minimum value of the input array, the length of the subarray on the right in the generated subarray is the input array length n-1. Therefore, the time complexity and space complexity are both O(n^2). This causes a stack overflow when testing an array of size 5000, so the upper limit of the array size when comparing an already sorted input array is 2000.
 
 
 - **1b.**
 
+The first comparison is the sorted array, and the result is shown in the following figure.
+![image](image.png)
+The next comparison is the out-of-order array, and the result is shown in the following figure
+![image](image_2.png)
+Looking at the above figure, you can see that in a sorted array, the speed of using quicksort is slower than that of selection sort, because the list is copied every time it recurs.
+
+In sorted arrays, qsort-random-pivot is the fastest
+
+In an out-of-order array, qsort-fixed-pivot is the fastest
+
+The times for selection sort and qsort-random-pivot are basically unchanged regardless of the array.
+
+qsort-fixed-pivot is very different between the two arrays, because in the case of random arrays, the time complexity of quicksort is O(nlogn), while in the case of sorted arrays it degenerates to O(n ^2)
 
 
 
 - **1c.**
+  ![image](image_3.png)
+In addition to the sort that comes with python, the fastest one is qsort-fixed-pivot. The main reason for such a large time gap is that the input parameters of qsort are limited and the list copy operation is required, and a large number of memory operations make the speed slower. 
--- main.py
+++ main.py
@@ -1,24 +1,55 @@
 import random, time
 import tabulate
-
+import sys
+sys.setrecursionlimit(100000)
 
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
-    
+    if len(a) == 0:
+        return []
+    pivot = pivot_fn(a)
+    idx = a.index(pivot)
+    a[idx], a[0] = a[0], a[idx]
+    i = 0
+    j = len(a) - 1
+    pivot = a[0]
+    while i < j:
+        while i < j and a[j] >= pivot:
+            j -= 1
+        if i < j and a[j] < pivot:
+            a[i] = a[j]
+        while i < j and a[i] < pivot:
+            i += 1
+        if i < j and a[i] >= pivot:
+            a[j] = a[i]
+    lef = qsort(a[:i], pivot_fn)
+    mid = [pivot]
+    rig = qsort(a[i + 1:], pivot_fn)
+    return lef + mid + rig
+
+
+def sSort(a):
+    for i in range(len(a)-1):
+        index_of_min = 0
+        for j in range(i, len(a)):
+            if a[j] < a[index_of_min]:
+                index_of_min = j
+        a[i], a[index_of_min] = a[index_of_min], a[i]
+    return a
+
+
 def time_search(sort_fn, mylist):
     """
     Return the number of milliseconds to run this
     sort function on this list.
 
     Note 1: `sort_fn` parameter is a function.
-    Note 2: time.time() returns the current time in seconds. 
+    Note 2: time.time() returns the current time in seconds.
     You'll have to multiple by 1000 to get milliseconds.
 
     Params:
       sort_fn.....the search function
       mylist......the list to search
-      key.........the search key 
+      key.........the search key
 
     Returns:
       the number of milliseconds it takes to run this
@@ -29,7 +60,8 @@ def time_search(sort_fn, mylist):
     return (time.time() - start) * 1000
     ###
 
-def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
+
+def compare_sort(sizes=[10, 20, 50, 100, 200, 500, 1000, 1500, 2000, 5000, 10000, 100000]):
     """
     Compare the running time of different sorting algorithms.
 
@@ -40,32 +72,38 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
+    qsort_fixed_pivot = lambda x: qsort(x, lambda y: y[0])
+    qsort_random_pivot = lambda x: qsort(x, lambda y: random.choice(y))
+    pythonSort = sorted
+    # tim_sort = #
     result = []
     for size in sizes:
         # create list in ascending order
         mylist = list(range(size))
         # shuffles list if needed
-        #random.shuffle(mylist)
+        random.shuffle(mylist)
         result.append([
             len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
+            time_search(qsort_fixed_pivot, mylist.copy()),
+            time_search(qsort_random_pivot, mylist.copy()),
+            time_search(pythonSort, mylist.copy()),
+            time_search(sSort, mylist.copy())
         ])
     return result
     ###
 
+
 def print_results(results):
     """ change as needed for comparisons """
     print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
+                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot', 'python sort', 'selection sort'],
                             floatfmt=".3f",
                             tablefmt="github"))
+
 
 def test_print():
     print_results(compare_sort())
 
-random.seed()
-test_print()
+#
+# random.seed()
+# test_print()
